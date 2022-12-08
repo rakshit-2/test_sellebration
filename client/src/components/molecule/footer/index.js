bf7e88689d6c347cc799e2ec29642c76fba076eb
rakshit-2 <rakhsit4013@gmail.com>
2022-12-08 20:11:30 +0530

    bug-sheet-1.1 fix alignment full media query

--- client/src/components/molecule/footer/index.js
+++ client/src/components/molecule/footer/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import { useNavigate } from 'react-router-dom';
 import FooterData from './../../assets/store/footerData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRegistered } from '@fortawesome/free-regular-svg-icons'
+import { faCopyright, faRegistered } from '@fortawesome/free-regular-svg-icons'
 
 const Footer=(props)=>{
     const navigate = useNavigate();
@@ -34,14 +34,14 @@ return (
                     const {id,img}=item;
                     return(
                         <div key={id} className='footer__inner__top__left__each2'>
-                            <FontAwesomeIcon icon={img} size="lg" color="white" style={{cursor:"pointer"}} />
+                            <FontAwesomeIcon icon={img} size="lg" color="white" className='icon__test__col' style={{cursor:"pointer"}} />
                         </div>
                     )
                 })}
                 </div>
             </div>
             <div className='footer__inner__mid'>
-                {FooterData.middle}{'\u00A0'}<FontAwesomeIcon icon={faRegistered} size="lg" color="white" style={{cursor:"pointer"}} />
+                {FooterData.middle}{'\u00A0'}<FontAwesomeIcon icon={faCopyright} size="lg" color="white" style={{cursor:"pointer"}} />
             </div>
             <div className="footer__inner__bottom">
                 {FooterData.bottom.map((item) => {
